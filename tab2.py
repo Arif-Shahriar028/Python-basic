e8c3d1b1759c9ed7a016315637c9fdf7697c9975
Arif Shahriar <arifshahriar028@gmail.com>
2020-12-04 17:20:04 +0600

    class and object

--- tab2.py
+++ tab2.py
@@ -12,8 +12,32 @@ class Class1(object):
         print(self.z)
 
 
+class Class2:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __del__(self):
+        print("object delete is called and this object is deleted")
+
+    def sum(self):
+        ans = self.y + self.z
+        print(f"{self.x} {ans}")
+
+
+# creat object of class1
 obj = Class1()
 obj.sum()
 obj.x = 30
 obj.y = 40
 obj.sum()
+
+# create object of class2
+obj2 = Class2("Arif", 13, 14)
+obj2.sum()
+
+del obj2
+
+
+
